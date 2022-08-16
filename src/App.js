04e6180ee64aa0c93228fc14dab256b53f6804f1
Mo <mdarker0@MacBook-Pro-de-Mo.local>
2022-08-16 13:13:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactSortable } from "react-sortablejs";
+import { useState } from 'react';
 
 function App() {
+
+  const [aze, setaze] = useState([
+    {
+      title: 'aze 1',
+      style: {height: '100px', width: '100px', border: '1px solid black'},
+    },
+    {
+      title: 'aze 2',
+      style: {height: '200px', width: '100px', border: '1px solid black'},
+    },
+    {
+      title: 'aze 3',
+      style: {height: '100px', width: '400px', border: '1px solid black'},
+    },
+    {
+      title: 'aze 4',
+      style: {height: '100px', width: '100px', border: '1px solid black'},
+    },
+    {
+      title: 'aze 5',
+      style: {height: '100px', width: '100px', border: '1px solid black'},
+    },
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactSortable list={aze} setList={setaze} className="App" style={{padding: '10px', display: 'flex', flexWrap: 'wrap', gap: '15px' , border: '1px solid black'}}>
+      {aze.map( (a,i) => <Aze a={a} key={i} /> )}
+      </ReactSortable>
   );
 }
 
 export default App;
+
+
+function Aze({a, i}) {
+  
+  return (
+    
+    <div style={a.style} >
+        {a.title}
+      </div>
+  )
+}
